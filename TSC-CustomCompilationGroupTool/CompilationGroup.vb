da3316722888cfadc28c6c4b53125e0f85696cee
Isaac Schemm <isaacschemm@gmail.com>
2017-04-20 11:13:51 -0500

    Define INI format and set up async tasks

---
+++ TSC-CustomCompilationGroupTool/CompilationGroup.vb
@@ -0,0 +1,54 @@
+﻿Public Class CompilationGroup
+    Public ReadOnly Property Name As String
+
+    Private ReadOnly Paths As HashSet(Of String)
+    Private ReadOnly Dependencies As HashSet(Of CompilationGroup)
+
+    Private CompilationTask As Task
+
+    Public Sub New(Name As String)
+        Me.Name = Name
+        Me.Paths = New HashSet(Of String)
+        Me.Dependencies = New HashSet(Of CompilationGroup)
+    End Sub
+
+    Public Sub Add(Path As String)
+        Paths.Add(Path)
+    End Sub
+
+    Public Sub Add(Group As CompilationGroup)
+        If Group.Dependencies.Contains(Me) Then
+            Throw New Exception($"Circular dependency detected: {Me} is already a dependency of {Group}")
+        End If
+        Dependencies.Add(Group)
+        For Each Dependency In Group.Dependencies
+            Dependencies.Add(Dependency)
+        Next
+    End Sub
+
+    Public Function Compile() As Task
+        SyncLock Me
+            If CompilationTask Is Nothing Then
+                CompilationTask = Me.CompileInternal()
+            End If
+            Return CompilationTask
+        End SyncLock
+    End Function
+
+    Private Async Function CompileInternal() As Task
+        Console.WriteLine("Start compile " & Name)
+        Dim CompilationTasks As New List(Of Task)
+        For Each Group In Dependencies
+            CompilationTasks.Add(Group.Compile())
+        Next
+        For Each Task In CompilationTasks
+            Await Task
+        Next
+        Await Task.Delay(2000)
+        Console.WriteLine("End compile " & Name)
+    End Function
+
+    Public Overrides Function ToString() As String
+        Return Name
+    End Function
+End Class
--- TSC-CustomCompilationGroupTool/Module1.vb
+++ TSC-CustomCompilationGroupTool/Module1.vb
@@ -1,7 +1,54 @@
-﻿Module Module1
+﻿Imports System.IO
+Imports System.Reflection
+
+Module Module1
 
     Sub Main()
+        Try
+            Dim InputFile = My.Application.CommandLineArgs.FirstOrDefault
+            If InputFile Is Nothing Then
+                InputFile = Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().Location) & ".ini"
+            End If
+            If Not File.Exists(InputFile) Then
+                Throw New Exception($"Could not find {InputFile}")
+                Return
+            End If
 
+            Dim Groups As New List(Of CompilationGroup)
+
+            Using InputStream As New FileStream(InputFile, FileMode.Open, FileAccess.Read)
+                Using Reader As New StreamReader(InputStream)
+                    Dim CurrentGroup As CompilationGroup = Nothing
+                    Dim CurrentLine = Reader.ReadLine()
+
+                    While CurrentLine IsNot Nothing
+                        If CurrentLine.StartsWith("[") And CurrentLine.EndsWith("]") Then
+                            CurrentGroup = New CompilationGroup(CurrentLine.Substring(1, CurrentLine.Length - 2))
+                            Groups.Add(CurrentGroup)
+                        ElseIf Not String.IsNullOrWhiteSpace(CurrentLine) And Not CurrentLine.StartsWith(";") Then
+                            Dim ExistingGroup = Groups.SingleOrDefault(Function(g) g.Name = CurrentLine)
+                            If ExistingGroup IsNot Nothing Then
+                                CurrentGroup.Add(ExistingGroup)
+                            ElseIf File.Exists(CurrentLine) Then
+                                CurrentGroup.Add(CurrentLine)
+                            Else
+                                Throw New Exception($"{CurrentLine} is not a TypeScript file or the name of a group earlier in the configuration file")
+                            End If
+                        End If
+
+                        CurrentLine = Reader.ReadLine()
+                    End While
+                End Using
+            End Using
+
+            Dim GroupAll As New CompilationGroup("META")
+            For Each Group In Groups
+                GroupAll.Add(group)
+            Next
+            GroupAll.Compile().Wait()
+        Catch e As Exception
+            MsgBox(e.Message, MsgBoxStyle.Critical, e.GetType().Name)
+        End Try
     End Sub
 
 End Module
